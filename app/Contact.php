e3958e8c25db2dbd5cb832b114d3543fb07eb05d
三浦真 <>
2020-05-02 18:37:43 +0900

    お問い合わせモデル作成

--- app/Contact.php
+++ app/Contact.php
@@ -4,7 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Contacts extends Model
+class Contact extends Model
 {
-    
+    protected $guarded = array('id');
+
+    public static $rules = array(
+        'title' => 'required|max:50',
+        'text' => 'required|max:300',
+    );
 }
