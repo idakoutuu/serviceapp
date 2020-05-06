90cdb49c4b706ce21433a11c09f165939aed50d4
三浦真 <>
2020-05-06 15:29:20 +0900

    ルート変更

--- resources/views/auth/verrification.blade.php
+++ resources/views/auth/verrification.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-10">
                 <div class="card">
-                    <form action="{{ route('auth.thanks') }}" method="post">
+                    <form action="{{ route('thanks') }}" method="post">
                         @csrf
                         <div class="col-md-6 offset-md-3 form-group col-form-label">
                         <label>名前</label>
