0834e7efe1154be65e16e1d4c1ea7e7b367f5770
三浦真 <>
2020-05-13 17:46:21 +0900

    確認ページレイアウト変更

--- resources/views/auth/verification.blade.php
+++ resources/views/auth/verification.blade.php
@@ -8,57 +8,57 @@
                     <form action="{{ route('register') }}" method="post">
                         @csrf
                         <div class="col-md-6 offset-md-3 form-group col-form-label">
-                        <label>名前</label>
+                        <label>名前:</label>
                         {{$inputs['name']}}
                         <input type="hidden" name="name" value="{{$inputs['name']}}" class="form-control">
                         </div>
 
                         <div class="col-md-6 offset-md-3 form-group col-form-label">
-                        <label>メールアドレス</label>
+                        <label>メールアドレス:</label>
                         {{$inputs['email']}}
                         <input type="hidden" name="email" value="{{$inputs['email']}}" class="form-control">
                         </div>
 
                         <div class="col-md-6 offset-md-3 form-group col-form-label">
-                        <label>パスワード</label>
+                        <label>パスワード:</label>
                         【表示されません】
                         <input type="hidden" name="password" value="{{$inputs['password']}}" class="form-control">
                         </div>
 
                         <div class="col-md-6 offset-md-3 form-group col-form-label">
-                        <label>性別</label>
+                        <label>性別:</label>
                         {{$inputs['gender']}}
                         <input type="hidden" name="gender" value="{{$inputs['gender']}}" class="form-control">
                         </div>
 
                         <div class="col-md-6 offset-md-3 form-group col-form-label">
-                        <label>生年月日</label>
-                        {{$inputs['birth_year']}} {{$inputs['birth_month']}} {{$inputs['birth_day']}}
+                        <label>生年月日:</label>
+                        {{$inputs['birth_year']}} 年 {{$inputs['birth_month']}} 月 {{$inputs['birth_day']}} 日
                         <input type="hidden" name="birth_year" value="{{$inputs['birth_year']}}" class="form-control">
                         <input type="hidden" name="birth_month" value="{{$inputs['birth_month']}}" class="form-control">
                         <input type="hidden" name="birth_day" value="{{$inputs['birth_day']}}" class="form-control">
                         </div>
 
                         <div class="col-md-6 offset-md-3 form-group col-form-label">
-                        <label>プロフィール写真</label>
+                        <label>プロフィール写真:</label>
                         {{$inputs['']}}
                         <input type="hidden" name="picture" value="{{$inputs['picture']}}" class="form-control">
                         </div>
 
                         <div class="col-md-6 offset-md-3 form-group col-form-label">
-                        <label>お住まい</label>
+                        <label>お住まい:</label>
                         {{$inputs['prefecture']}}
                         <input type="hidden" name="prefecture" value="{{$inputs['prefecture']}}" class="form-control">
                         </div>
 
                         <div class="col-md-6 offset-md-3 form-group col-form-label">
-                        <label>趣味</label>
+                        <label>趣味:</label>
                         {{$inputs['hobby']}}
                         <input type="hidden" name="hobby" value="{{$inputs['hobby']}}" class="form-control">
                         </div>
 
                         <div class="col-md-6 offset-md-3 form-group col-form-label">
-                        <label>職業</label>
+                        <label>職業:</label>
                         {{$inputs['profession']}}
                         <input type="hidden" name="profession" value="{{$inputs['profession']}}" class="form-control">
                         </div>
