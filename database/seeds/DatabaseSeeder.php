0b6a6b4ebcb1392e00a49cd1664eeb068fe40432
三浦真 <>
2020-05-18 22:25:24 +0900

    シード処理変更

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
-        //$this->call(PrefecturesTableSeeder::class);
-        //$this->call(HobbiesTableSeeder::class);
+        //$this->call(UsersTableSeeder::class);
+        $this->call(PrefecturesTableSeeder::class);
+        $this->call(HobbiesTableSeeder::class);
+        //$this->call(ProfessionsTableSeeder::class);
+
     }
 }
