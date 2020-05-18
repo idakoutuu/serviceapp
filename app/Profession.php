2fc48a93943353bf4e24b2d3546b0cd08e6a614f
三浦真 <>
2020-05-18 22:22:23 +0900

    職業モデル作成

---
+++ app/Profession.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profession extends Model
+{
+    protected $guarded = array('id');
+
+    public static $rules = array(
+        'profession_name' => 'required'
+    );
+
+    public function user()
+    {
+        return $this->hasOne('App\user');
+    }
+}
