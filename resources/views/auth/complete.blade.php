f8959039d88345a3b5367546f304744fdeba5313
三浦真 <>
2020-05-31 17:20:19 +0900

    新規登録完了ブレード作成

--- resources/views/auth/complete.blade.php
+++ resources/views/auth/complete.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.app')
-
+<style>
+    .complete {
+        text-align : center;
+    }
+    .top-page {
+        font-size: 2rem;
+    }
+</style>
 @section('content')
-    登録完了しました。
+    <div class="complete">
+        <h2>登録が完了しました！</h2>
+        <a href="{{ route('user.usertop') }}" class="top-page">【トップページへ】</a>
+    </div>
 @endsection
