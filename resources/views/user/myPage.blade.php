861821855298228273cd892a1d560fe866e06c14
三浦真 <>
2020-06-09 21:13:11 +0900

    マイページブレード変更

--- resources/views/user/myPage.blade.php
+++ resources/views/user/myPage.blade.php
@@ -10,7 +10,19 @@
     @section('title')
     @section('content')
         <div class="page">
-            name:                      <span>{{ $auth->name }}</span>
+            プロフィール写真:
+            @if ($auth->photograph == null)
+            <img src="{{ asset('/storage/images/noimage.jpg') }}" alt="noimage" width="150px" height="100px">
+            @else
+            <img src="{{ asset('/storage/images/' . $auth->photograph) }}" alt="">
+            @endif
+            <br>
+            名前:<span>{{ $auth->name }}</span><br>
+            性別:<span>{{ $auth->gender }}</span><br>
+            生年月日:<span>{{ $auth->birth }}</span><br>
+            都道府県:<span>{{ $auth->prefecture['prefecture_name'] }}</span><br>
+            趣味:<span>{{ $auth->hobby }}</span><br>
+            職業:<span>{{ $auth->profession['profession_name'] }}</span>
         </div>
     @endsection
 </body>
